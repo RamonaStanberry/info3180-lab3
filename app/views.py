0e536f406886bbbe7689ead7fef3c7205bbd84f0
RamonaStanberry <monacstanberry@gmail.com>
2022-02-19 14:55:00 -0500

    Lab 3

--- app/views.py
+++ app/views.py
@@ -5,9 +5,15 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 
+import email
+from email import message
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
+from app.forms import ContactForm
+
+from app import mail
+from flask_mail import Message
 
 ###
 # Routing for your application.
@@ -24,6 +30,25 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+
+@app.route('/contact', methods=["GET","POST"])
+def contact():
+    thisForm=ContactForm()
+    if request.method== 'POST':
+        if thisForm.validate_on_submit():
+            subject=thisForm.subject.data
+            name= thisForm.name.data
+            email=thisForm.email.data
+            mess=thisForm.message.data
+            msg = Message(subject, sender=(name,
+email),recipients=["to@example.com"]) 
+            msg.body = mess
+            mail.send(msg)
+
+            flash('Email sent successfully!','success')
+            return redirect(url_for('home'))
+
+    return render_template('contact.html',form=thisForm)
 
 ###
 # The functions below should be applicable to all Flask apps.
